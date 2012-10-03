d982b00c515972e26af9284eb46fa53e0e87443e
Adam Gilman <adam.gilman@gmail.coma>
2012-10-03 15:01:39 +0100

    TfL codes for stations and lines

---
+++ tflCodes/lines.py
@@ -0,0 +1,10 @@
+lines = {'B': ' Bakerloo',
+		 'C': ' Central',
+		 'D': ' District',
+		 'H': ' Hammersmith & Circle',
+		 'J': ' Jubilee',
+		 'M': ' Metropolitan',
+		 'N': ' Northern',
+		 'P': ' Piccadilly',
+		 'V': ' Victoria',
+		 'W': ' Waterloo & City'}
